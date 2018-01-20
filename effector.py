8b6be9d4d9963c1e047d40d374a9683a375e4896
sihadan <def6488@gmail.com>
2018-01-20 22:14:34 +0900

    fix drawing algorithm

--- effector.py
+++ effector.py
@@ -53,7 +53,7 @@ def snow_lane(img, n_lanes=40, n_frames=50, min_speed=(-2, 2), speed_deviation=(
     for lane in lanes:
         positions = lane.get_positions()
         particle_size, speed_x, speed_y = lane.get_lane_info()
-        color = random.choice(color_palette.DAWN)
+        color = random.choice(color_palette.BRIGHT)
 
         for pos in positions:
             snow = Snow(
@@ -71,15 +71,16 @@ def snow_lane(img, n_lanes=40, n_frames=50, min_speed=(-2, 2), speed_deviation=(
     for i in range(n_frames):
 
         frame = img.copy()
-        effect_mask = Image.new('RGBA', img.size, color=(255, 255, 255))  # image for masking
-        draw_board = ImageDraw.Draw(effect_mask)
 
         for p in snow_particles:
-            p.draw(draw_board)  # draw new particle
+            mask = Image.new('RGBA', img.size, color=(255, 255, 255))  # image for masking
+            board = ImageDraw.Draw(mask)
+
+            p.draw(board)
             p.move()
 
-        # frame.paste(effect_mask, mask=effect_mask)
-        frame = Image.composite(frame, effect_mask, mask=effect_mask)
+            frame = Image.composite(frame, mask, mask=mask)  # add particle to frame
+
         frames.append(frame)
 
     return frames
